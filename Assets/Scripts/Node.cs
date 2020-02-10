af68aee8ce9730b7908c4d0d5bf27732ee8e13bf
MarcinGała <marcingala81@gimail.com>
2020-02-10 01:07:21 +0100

    add project

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    public bool Walkable;
+    public Vector3 WorldPosition;
+    public int GridX;
+    public int GridY;
+
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    public Node(bool walkable, Vector3 worldPos, int gridX, int gridY)
+    {
+        Walkable = walkable;
+        WorldPosition = worldPos;
+        GridX = gridX;
+        GridY = gridY;
+    }
+
+    public int fCost //we want only get the fcost from return gCost + hCost
+    {
+        get 
+        {
+            return gCost + hCost;
+        }
+    }
+}
